b8c004190e89585de726ecbbd65e8bbbd21dee4d
Jean-Marcien Kablansi <jean.marcien@gmail.com>
2024-10-16 23:58:29 -0400

    Submissions

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,15 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  get 'restaurants', to: 'restaurants#index'
-  get 'restaurants/new', to: 'restaurants#new'
-  post 'restaurants', to: 'restaurants#create'
-  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+  # get 'restaurants', to: 'restaurants#index'
+  # get 'restaurants/new', to: 'restaurants#new'
+  # post 'restaurants', to: 'restaurants#create'
+  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
 
+  # get 'restaurants/:restaurant_id/reviews/new', to: 'reviews#new'
+  # post 'restaurants/:restaurant_id/reviews', to: 'reviews#create'
+
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
 end
